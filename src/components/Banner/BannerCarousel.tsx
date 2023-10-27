bfef9fa921ced752782431666b070d467fdbf402
Jihyun <wlgus0260@naver.com>
2023-10-27 16:37:02 +0900

    style: 배너 이미지, CSS 수정

--- src/components/Banner/BannerCarousel.tsx
+++ src/components/Banner/BannerCarousel.tsx
@@ -17,33 +17,39 @@ export default function BannerCarousel(): React.ReactElement {
       showThumbs={false}
     >
       <div className={styles.carouselContainer}>
-        <img src="https://react-shop-oinochoe.vercel.app/img_shop_fashion.jpeg" alt="banner1" />
-        <div className={styles.des}>
-          <h2 className="title-bold-24">물 빠진 청바지!</h2>
-          <p className="text-regular-16 mtb">이제 막 도착한 패션 청바지를 구경해 보세요.</p>
-          <Link to="/clothing">
-            <button className={styles.moveBtn}>바로가기 ➜</button>
-          </Link>
+        <img src="/images/fashion.jpeg" alt="banner1" />
+        <div className={styles.desContainer}>
+          <div className={styles.des}>
+            <h2 className="title-bold-24">물 빠진 청바지!</h2>
+            <p className="text-regular-16 mtb">이제 막 도착한 패션 청바지를 구경해 보세요.</p>
+            <Link to="/clothing">
+              <button className={styles.moveBtn}>바로가기 ➜</button>
+            </Link>
+          </div>
         </div>
       </div>
       <div className={styles.carouselContainer}>
-        <img src="https://react-shop-oinochoe.vercel.app/img_shop_digital.jpeg" alt="banner2" />
-        <div className={styles.des}>
-          <h2 className="title-bold-24">신속한 업무처리!</h2>
-          <p className="text-regular-16 mtb">다양한 디지털 상품을 둘러보세요.</p>
-          <Link to="/electronics">
-            <button className={styles.moveBtn}>바로가기 ➜</button>
-          </Link>
+        <img src="/images/digital.jpeg" alt="banner2" />
+        <div className={styles.desContainer}>
+          <div className={styles.des}>
+            <h2 className="title-bold-24">신속한 업무처리!</h2>
+            <p className="text-regular-16 mtb">다양한 디지털 상품을 둘러보세요.</p>
+            <Link to="/electronics">
+              <button className={styles.moveBtn}>바로가기 ➜</button>
+            </Link>
+          </div>
         </div>
       </div>
       <div className={styles.carouselContainer}>
-        <img src="	https://react-shop-oinochoe.vercel.app/img_shop_grocery.jpeg" alt="banner3" />
-        <div className={styles.des}>
-          <h2 className="title-bold-24">신선한 식품!</h2>
-          <p className="text-regular-16 mtb">농장 직배송으로 더욱 신선한 식료품을 만나보세요.</p>
-          <Link to="/grocery">
-            <button className={styles.moveBtn}>바로가기 ➜</button>
-          </Link>
+        <img src="/images/grocery.jpeg" alt="banner3" />
+        <div className={styles.desContainer}>
+          <div className={styles.des}>
+            <h2 className="title-bold-24">신선한 식품!</h2>
+            <p className="text-regular-16 mtb">농장 직배송으로 더욱 신선한 식료품을 만나보세요.</p>
+            <Link to="/grocery">
+              <button className={styles.moveBtn}>바로가기 ➜</button>
+            </Link>
+          </div>
         </div>
       </div>
     </Carousel>
